16ed831e2fa22191ae6f9d0f4b2887c6bd9eeec4
Francesca Giordano <francesca020406@hotmail.com>
2017-11-13 19:58:31 -0500

    yay backend

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
+var phonesApi = require('./routes/phones-api');
+
+// database connection
+require('./configs/database');
+
 
 var app = express();
 
@@ -23,7 +28,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
+app.use('/api', phonesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/phones-api.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+
+const Phone = require('../models/phone-model');
+
+/* GET Phones listing. */
+router.get('/phones', (req, res, next) => {
+  Phone.find((err, phonesList) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+    res.json(phonesList);
+  });
+});
+
+/* CREATE a new Phone. */
+router.post('/phones', (req, res, next) => {
+  const thePhone = new Phone({
+    brand: req.body.brand,
+    name: req.body.name,
+    specs: req.body.specs,
+    image: req.body.image || ''
+  });
+
+  thePhone.save((err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    res.json({
+      message: 'New Phone created!',
+      id: thePhone._id
+    });
+  });
+});
+
+/* GET a single Phone. */
+router.get('/phones/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+  
+  Phone.findById(req.params.id, (err, thePhone) => {
+      if (err) {
+        res.json(err);
+        return;
+      }
+
+      res.json(thePhone);
+    });
+});
+
+/* EDIT a Phone. */
+router.put('/phones/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  const updates = {
+    brand: req.body.brand,
+    name: req.body.name,
+    specs: req.body.specs,
+    image: req.body.image
+  };
+  
+  Phone.findByIdAndUpdate(req.params.id, updates, (err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    res.json({
+      message: 'Phone updated successfully'
+    });
+  });
+})
+
+/* DELETE a Phone. */
+router.delete('/phones/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+  
+  Phone.remove({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    return res.json({
+      message: 'Phone has been removed!'
+    });
+  })
+});
+
+module.exports = router;
